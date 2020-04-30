db5fb87681de5c4dc08eb10fb603345a1ca89b1a
Hailey Bae <haejinbae@gmail.com>
2020-04-29 21:25:56 -0400

    updating homework to the most recent edits

--- index.js
+++ index.js
@@ -6,5 +6,167 @@
 
 * Update employee roles */
 
-v
+var db = require("./db/connection");
 var inquirer = require("inquirer");
+/*const viewOptions = [
+    "View All Employees",
+    "View All Employees By Department",
+    "View All Employees By Manager",
+    "Add Employee",
+    "Remove Employee",
+    "Update Employee Role",
+    "Update Employee Manager",
+    "View All Roles",
+    "Exit"
+];
+
+const employeeOptions = [
+    "Kim Stewart",
+    "Bob Ross",
+    "Justiene Cliff",
+    "Zachary Lee",
+    "David Lee",
+    "James Stamos",
+    "Laurie Sheuffer",
+    "Kirstin Verden",
+    "Exit"
+];
+
+const updateEmployee = [
+    "First Name",
+    "Last Name",
+    "Role",
+    "Exit"
+]*/
+
+
+function userQuestion () {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Employees By Department",
+          "View All Employees By Manager",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Manager",
+          "View All Roles"
+        ]
+    }).then(function(answer) {
+        switch (answer.choices) {
+            case "View All Employees":
+                viewAllEmployees();
+                console.log(answer.choices);
+                break;
+                
+            case "View All Employees By Department":
+                viewEmployeesDepartment();
+                console.log(answer.choices);
+                break;
+
+            case "View All Employees By Manager":
+                viewEmployeesManager();
+                console.log(answer.choices);
+                break;
+
+            case "Add Employee":
+                addEmployee();
+                console.log(answer.choices);
+                break;
+
+            case "Remove Employee":
+                removeEmployee();
+                console.log(answer.choices);
+                break;
+
+            case "Update Employee Role":
+                updateEmployeeRole();
+                console.log(answer.choices);
+                break;
+
+            case "Update Employee Manager":
+                updateEmployeeManager();
+                console.log(answer.choices);
+                break;
+
+            case "View All Roles":
+                allRoles();
+                console.log(answer.choices);
+                break;
+
+        }
+    });
+}
+
+userQuestion();
+
+function viewAllEmployees () {
+    db.query('SELECT * FROM employee', function (error, results, fields) {
+        if (error) throw error;
+    console.log('The solution is: ', results);
+});
+viewAllEmployees();
+}
+
+function viewEmployeesDepartment () {
+    db.query('SELECT * FROM department', function (error, results, fields) {
+        if (error) throw error;
+    console.log('The solution is: ', results);
+    userQuestion();
+});
+}
+
+function viewEmployeesManager () {
+    db.query('SELECT * FROM employee', function (error, results, fields) {
+        if (error) throw error;
+    console.log('The solution is: ', results);
+    userQuestion();
+
+});
+}
+
+function addEmployee () {
+    db.query('SELECT * FROM employee', function (error, results, fields) {
+        if (error) throw error;
+    console.log('The solution is: ', results);
+    userQuestion();
+
+});
+}
+
+function removeEmployee () {
+    db.query('SELECT * FROM employee', function (error, results, fields) {
+        if (error) throw error;
+    console.log('The solution is: ', results);
+    userQuestion();
+});
+}
+
+function updateEmployeeRole () {
+    db.query('SELECT * FROM employee', function (error, results, fields) {
+        if (error) throw error;
+    console.log('The solution is: ', results);
+    userQuestion();
+});
+}
+
+
+function updateEmployeeManager () {
+    db.query('SELECT * FROM employee', function (error, results, fields) {
+        if (error) throw error;
+    console.log('The solution is: ', results);
+    userQuestion();
+});
+}
+
+function allRoles () {
+    db.query('SELECT * FROM employee', function (error, results, fields) {
+        if (error) throw error;
+    console.log('The solution is: ', results);
+    userQuestion();
+});
+}
+//create a different function for each case listed in line 35- 61
